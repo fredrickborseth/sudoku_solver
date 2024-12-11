b312bb3198ccf45dbf97d7c86bc6806d73b1ad91
fredrickborseth <fredrick.boerseth@proton.me>
2024-12-11 16:26:00 +0100

    Update sudoku_solver.py
    added function: how_many_zeros()
        - makes it possible to skip over the number group with more than one zero.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -15,23 +15,25 @@ r9 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 rows = [r1, r2, r3, r4, r5, r6, r7, r8, r9]
 
-def manipulate_zero_index(num_group):
-    n = finding_missing_number(num_group)
-    working_num_list = num_group.index(0)
-    num_group.insert(working_num_list, n)
-    num_group.remove(0)
-    print(num_group)
-    
-
 def finding_missing_number(nums):
     n = len(nums) + 1
     total = sum([num for num in range(0, n)])
     return total - sum(nums)
-    
 
-def the_sudoku_format(x):
-    for nums in rows:
-        print(nums)
+def how_many_zeros(num_group):
+    set_of_nums = set(num_group)
+    length_of_set = len(set_of_nums)
+    if length_of_set == 9:
+        return finding_missing_number(num_group)
+    else:
+        return 0 # change this to something useful
+
+def manipulate_zero_index(num_group):
+    n = how_many_zeros(num_group)
+    working_num_list = num_group.index(0)
+    num_group.insert(working_num_list, n) # needs a way to find the missing number in the list
+    num_group.remove(0)  # removing the zero in the list
+    return num_group
 
 
 manipulate_zero_index(r1)
